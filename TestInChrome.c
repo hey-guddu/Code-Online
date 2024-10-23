3e132f76d72f842ffd5557472328598e10783be2
Guddu <155829795+hey-guddu@users.noreply.github.com>
2024-10-24 01:50:58 +0530

    Code Update

--- TestInChrome.c
+++ TestInChrome.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    print("Hello World..!!");
+    printf("Let's start coding");
+    int first_number, second_number, temp_value;
+    printf("Enter first number: ");
+    scanf("%d", &first_number);
+    printf("Enter second number: ");
+    scanf("%d", &second_number);
+    printf("Before swap, \nFirst number is: %d,\t Second number is: %d", first_number, second_number);
+    temp_value = first_number;
+    first_number = second_number;
+    second_number = temp_value;
+    printf("After swap, \nFirst number is: %d,\t Second number is: %d", first_number, second_number);
+    printf("Program ends here..!!");
     return 0;
 }
